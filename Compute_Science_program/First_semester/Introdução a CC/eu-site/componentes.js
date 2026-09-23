1f3180dfa1867e94204d60f5331c4b922fafbd95
João Couto <joaocouto0001@gmail.com>
2026-09-23 01:40:42 -0300

    Delete Compute_Science_program/First_semester/Introdução a CC directory

--- Compute_Science_program/First_semester/Introdução a CC/eu-site/componentes.js
+++
@@ -1,9 +0,0 @@
-fetch("componentes/github-card.html") // Certifique-se de que o arquivo se chama github-card.html
-    .then(resposta => {
-        if (!resposta.ok) throw new Error("Erro ao carregar o arquivo HTML");
-        return resposta.text();
-    })
-    .then(dados => {
-        document.getElementById("github-container").innerHTML = dados;
-    })
-    .catch(erro => console.error("Erro no Fetch:", erro));
